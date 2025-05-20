a571f1a24a585f0f6409ffa0844cc61b9ca25e51
shafaasalsabila00 <sashaaraabil@gmail.com>
2025-05-20 11:00:53 +0700

    menambahkan output isi stack

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -1,3 +1,67 @@
 #include <iostream>
 using namespace std;
 
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node()
+    {
+        next = NULL;
+    }
+};
+
+class Stack
+{
+
+private:
+    Node *top;
+
+public:
+    Stack()
+    {
+        top = NULL;
+    }
+
+    int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push Value: " << value << endl;
+        return value;
+    }
+
+    void pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is Empty" << endl;
+        }
+
+        cout << "Popped Value: " << top->data << endl;
+        top = top->next;
+    }
+
+    void peek()
+    {
+        if (top == NULL)
+        {
+            cout << "List is Empty." << endl;
+        }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
+
+    
