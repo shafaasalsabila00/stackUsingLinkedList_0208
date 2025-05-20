2b0ae43c10e32fb821c5ef819f1b121686aae5f5
shafaasalsabila00 <sashaaraabil@gmail.com>
2025-05-20 10:17:53 +0700

    menambahkan fungsi peek

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -46,5 +46,21 @@ public:
         top = top->next;
     }
 
-    
+    void peek()
+    {
+        if (top == NULL)
+        {
+            cout << "List is Empty." << endl;
+        }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        }
+    }
 };
